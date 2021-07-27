33dcf532863931c8aad182532422744dca2e3463
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-07-27 17:25:17 -0600

    tailwind styling and sending messages, with some typing detection

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const io = require("socket.io")(3000,{
     },
 });
 
+
 io.on('connection', socket =>{
-    console.log('New User')
-    socket.emit('chat-message')
-    socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', message)
+    socket.on('new-user', name =>{
+        socket.broadcast.emit('user-connected', name);
+    });
+
+    socket.on('send-chat-message', (message, username) => {
+        socket.broadcast.emit('chat-message', message, username)   
+   
+    });
+
+    socket.on('someone-typing', (username)=>{
+        socket.broadcast.emit('someone-typing', username)
     })
 });
--- script.js
+++ script.js
@@ -2,20 +2,72 @@ const socket = io('http://localhost:3000');
 const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
+const username = prompt("Please Provide a Name");
 
-socket.on('chat-message', data => {
-    appendMessage(data);
+appendMessage("You just joined the room, say Hi!")
+
+//SOCKETS EVENTS
+socket.emit('new-user', username)
+
+socket.on('user-connected', name => {
+    if (name === null) {
+        name = "Guest"
+    }
+    
+    newUser(`${name} just joined!, say hi`);        
+});
+
+socket.on('chat-message', (data, name) => {
+    if (name === null) {
+        name = "Guest"
+    }
+        appendMessage(name+": "+data);          
+});
+
+socket.on('someone-typing', (username) =>{
+    if (username === null) {
+        username = "Guest"
+    }
+    appendAlert(username);
+});
+
+
+//FUNCTIONS AND HTML EVENTS 
+messageInput.addEventListener('focus', ()=>{    
+    socket.emit('someone-typing', username)
 });
 
 messageForm.addEventListener('submit', e=>{
     e.preventDefault()
     const message = messageInput.value;
-    socket.emit('send-chat-message', message)
+    socket.emit('send-chat-message', message, username)
+
+    appendMessage(message)
     messageInput.value = '';
-})
+
+});
 
 function appendMessage(message){
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
+    messageContainer.append(messageElement);
+}
+
+function appendAlert(username){
+    const messageElement = document.createElement('p');
+    messageElement.setAttribute("id", "div-text-gray");
+    if (!document.getElementById("div-text-gray")) {
+        messageElement.classList.add('text-gray-500')
+        messageElement.innerText = username +" is typing...";
+        messageContainer.append(messageElement);
+    }
+
+}
+
+function newUser(name){
+    const messageElement = document.createElement('div');
+    messageElement.style.backgroundColor = "green";
+    messageElement.style.color = "#fff"; 
+    messageElement.innerText = name;
     messageContainer.append(messageElement);
 }
