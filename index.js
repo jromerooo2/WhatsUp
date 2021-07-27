08809d720e4be7fa6bcb536cf5d64f153dc57c3b
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-07-27 09:28:50 -0600

    showing messages on the others connected to the server

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const io = require("socket.io")(3000,{
 });
 
 io.on('connection', socket =>{
-    socket.emit('chat-message', 'Hello from socket server')
+    console.log('New User')
+    socket.emit('chat-message')
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', message)
+    })
 });
